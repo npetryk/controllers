7a09eefe49f754919c30e2dc29917eaa41bd737b
Nathan Petryk <nathanpetryk@gmail.com>
2019-05-14 20:15:41 -0500

    Rejiggered some things

--- src/pi_pd/core.clj
+++ src/pi_pd/core.clj
@@ -3,22 +3,37 @@
             [incanter
              [core :as incanter]
              [stats :as i-stats]
-             [charts :as i-charts]]
-            ))
+             [charts :as i-charts]]))
 
-(def ^:dynamic *ds* (atom []))
+(defn clamp
+  [mx n mn]
+  (if n
+    (min (max n mn) mx)
+    mn))
 
-(defn drop-prob
-  [{:keys [gain buffer-size Qref pi pd p]}
+(defn perc
+  [n]
+  (float (* 100 (min (max n -1) 1))))
+
+(def ^:dynamic *ds*
+  (atom []))
+(def ^:dynamic *consts*
+  nil)
+
+(defn pi-pd
+  [{:keys [pd pi Qk+1 p]}
    Qsamples]
-  (let [pd-d-1 pd
-        pd-i-1 pi
-        α      gain
-        B      buffer-size
+  (let [{α :gain
+         B :buffer-size
+         Qref :Qref} *consts*
+
+        pd-1 pd
+        pi-1 pi
+
         Qk     (nth Qsamples 0)
         Qk-1   (nth Qsamples 1)
 
-        pd-i-0
+        pi-0
         (cond-> (- (/ (+ Qk Qk-1)
                       2)
                    Qref)
@@ -26,8 +41,8 @@
           (/ Qref)
           true
           (* α)
-          pi
-          (+ pi))
+          pi-1
+          (+ pi-1))
 
         Qk+1 (let [Yi (fn [i]
                         (- (nth Qsamples i)
@@ -42,145 +57,121 @@
                    Sk-1 (Si 1)
                    S    (* Sk Sk-1)
                    Yk+1 (cond
-                         (= S 1) (* Yk (/ Yk (if (zero? Yk-1) 1 Yk-1)))
-                         :else Yk)]
+                          (= S 1) (* Yk (/ Yk (if (zero? Yk-1) 1 Yk-1)))
+                          :else Yk)]
                (+ Yk+1 Qk))
 
-        pd-d-0
-        (cond-> (/ (- Qk+1 Qref)
-                   (- B Qref))
-          true
-          (* α)
-          pd
-          (+ pd))]
-    {:pd pd-d-0
-     :pi pd-i-0
-     :Qk+1 Qk+1
-     :p (+ pd-d-0 pd-i-0)}))
+        pd-0
+        (cond-> (* (/ (- Qk+1 Qref)
+                      (- B Qref))
+                   #_α)
+          pd-1
+          (+ pd-1))]
 
-(defprotocol AsyncController)
+    {:pd pd-0
+     :pi pi-0
+     :Qk+1 Qk+1
+     :p (+ pd-0 pi-0)}))
 
 (defn drop-rate-control-loop
-  [output-atom sample-fn {:keys [Ts] :as consts}]
+  [output-atom sample-fn prob-fn]
   (let [ultimate-stime (System/currentTimeMillis)]
     (loop [samples []
            pd-1 {}]
       (let [stime (System/currentTimeMillis)
             sample (sample-fn)
-            pd-1 (if (>= (count samples) 4)
-                   (drop-prob consts samples)
-                   {:p 0, :pi 0, :pd 0})
+            ; pd-1 (if (>= (count samples) 4)
+            ;        (prob-fn {} samples)
+            ;        {})
             samples (-> (into () (reverse (take 3 samples)))
                         (conj sample))
             pd-0 (if (>= (count samples) 4)
-                   (drop-prob (merge consts pd-1) samples)
-                   {:p 0, :pi 0, :pd 0})
-            data (assoc pd-0
-                        :samples samples
-                        :t (- (System/currentTimeMillis) ultimate-stime)
-                        :sample sample
-                        :perc (perc (:p pd-0))
-                        :perci (perc (:pi pd-0))
-                        :percd (perc (:pd pd-0))
-                        )]
-        (reset! output-atom data)
-        (swap! *ds* conj data)
-        (Thread/sleep (- Ts (- (System/currentTimeMillis) stime)))
-        (recur samples pd-0)))))
+                   (prob-fn {}  #_pd-1 samples)
+                   {:p 0})
+            pd-0-data (assoc pd-0
+                             :t (- (System/currentTimeMillis) ultimate-stime)
+                             :sample sample)]
+        (reset! output-atom pd-0-data)
+        (swap! *ds* conj pd-0-data)
+        (Thread/sleep (- (:Ts *consts*) (- (System/currentTimeMillis) stime)))
+        (recur
+         samples
+         pd-0)))))
 
-(defn clamp
-  [mx n mn]
-  (if n
-    (min (max n mn) mx)
-    mn))
-
-(defn perc
-  [n]
-  (float (* 100 (min (max n 0) 1))))
-
-(defn pipd-abq
-  [{:keys [Qref gain buffer-size] :as consts}]
+(defn abq
+  [{:keys []}]
   (let [_drop-rate
         (atom {:p 0})
 
         arb
         (proxy [java.util.concurrent.ArrayBlockingQueue] [1000 #_buffer-size]
-         (add [e]
-           (let [dr @_drop-rate]
-             (if (or (> 1e-1 (:p dr))
-                     (> (Math/random) (:p dr)))
-               (do #_(printf "YEP  - %3.0f%% - %s\n"
-                           (perc (:p  dr))
-                           (specter/transform [specter/MAP-VALS number?] (comp int (partial * 100)) dr))
-                   (proxy-super add e))
-               (do #_(printf "NOPE - %3.0f%% - %s\n"
-                           (perc (:p dr))
-                           (specter/transform [specter/MAP-VALS number?] (comp int (partial * 100)) dr))
-                   (throw (IllegalStateException. "dropped")))))))]
+          (add [e]
+            (let [dr @_drop-rate]
+              (if (or (> 1e-1 (:p dr))
+                      (> (Math/random) (:p dr)))
+                (proxy-super add e)
+                (throw (IllegalStateException. "dropped"))))))]
 
     (future (drop-rate-control-loop
              _drop-rate
              #(.size arb)
-             consts))
+             (:control-fn *consts*)))
     arb))
 
+(defn simulate+graph [iterations]
+  (let [push-speed [1 5]
+        pull-speed [5 10]
+
+        abq (abq {})]
+
+    (Thread/sleep (* 4 (:Ts *consts*)))
+
+    (future
+      (doseq [_ (range (int (/ iterations 4)))]
+        (Thread/sleep (int
+                       (+ (* (- (second pull-speed) (first pull-speed))
+                             (Math/random))
+                          (first pull-speed))))
+        (.poll abq)))
+
+    (doseq [i (range iterations)]
+      (Thread/sleep (int
+                     (+ (* (- (second push-speed) (first push-speed))
+                           (Math/random))
+                        (first push-speed))))
+      (try
+        (.add abq 5)
+        (catch Exception e
+          nil)))
+
+    (let [ds (map
+              (fn [d]
+                (-> d
+                    (update :p perc)
+                    (update :pi (fnil perc 0))
+                    (update :pd (fnil perc 0))
+                    (update :Qk+1 (fnil identity 0))
+                    ))
+              @*ds*)]
+      (incanter/with-data
+        (incanter/to-dataset ds)
+        (-> (doto (i-charts/xy-plot :t :p :legend true)
+              (i-charts/add-lines :t :sample)
+              (i-charts/add-lines :t :pi)
+              (i-charts/add-lines :t :pd)
+              (i-charts/add-lines :t :p)
+              (i-charts/add-lines :t :Qk+1)
+              )
+            (incanter/view)
+            #_(incanter/save
+               "/home/nathan/smbshare/imgs/test.png"))))))
 
 (comment
 
- (binding [*ds* (atom [])]
-
-   (let [push-speed [1 2]
-         pull-speed [2 3]
-
-         Qref 50
-         gain 1/10
-         C (/ 1 (min (second push-speed) (second pull-speed)))
-         Ts (* (/ Qref C)
-               2)
-
-         abq (pipd-abq
-              {:gain gain
-               :buffer-size (* 4 Qref)
-               :Ts Ts
-               :Qref Qref})
-         its 5000]
-
-    (Thread/sleep (* 4 Ts))
-
-    (future
-     (doseq [_ (range its)]
-       (Thread/sleep (int
-                      (+ (* (- (second pull-speed) (first pull-speed))
-                            (Math/random))
-                         (first pull-speed))))
-       (.poll abq)))
-
-    (reduce
-     (fn [successes i]
-       (Thread/sleep (int
-                      (+ (* (- (second push-speed) (first push-speed))
-                            (Math/random))
-                         (first push-speed))))
-       (try
-        (.add abq 5)
-        (inc successes)
-        (catch Exception e
-          successes)))
-     0
-     (range its)))
-
-    (def ds @*ds*)
-
-    (incanter/with-data
-     (incanter/to-dataset ds)
-     (incanter/save
-      (doto (i-charts/xy-plot :t :perc :legend true)
-        (i-charts/add-lines :t :perc)
-        (i-charts/add-lines :t :perci)
-        (i-charts/add-lines :t :percd)
-        (i-charts/add-lines :t :sample)
-        )
-      "/home/nathan/smbshare/imgs/test.png")))
-
- )
-
+  (binding [*ds* (atom [])
+            *consts* {:gain 1/10
+                      :buffer-size 100
+                      :Qref 50
+                      :Ts 200
+                      :control-fn pi-pd}]
+    (simulate+graph 5000)))
